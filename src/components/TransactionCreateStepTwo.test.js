c62e2ce123bb2dceae2e78b022d3bcbd15c538d8
Vitor Hugo <vitork42@gmail.com>
2022-04-13 23:40:33 -0300

    :tada: Initial commit

---
+++ src/components/TransactionCreateStepTwo.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import TransactionCreateStepTwo from './TransactionCreateStepTwo'
+
+
+test('on initial render, the pay button is disabled', async () => {
+  var sender = {id: 5}
+  var receiver = {id: '5'}
+  
+  render(<TransactionCreateStepTwo sender={sender} receiver={receiver} />);
+
+  expect(await screen.findByRole('button', {name: /pay/i})).toBeDisabled();
+})
+
+test('if an amount and note is entered, the pay button becomes enabled', async () => {
+  var sender = {id: 5}
+  var receiver = {id: '5'}
+  
+  render(<TransactionCreateStepTwo sender={sender} receiver={receiver} />);
+
+  userEvent.type(screen.getByPlaceholderText(/amount/i), '50')
+  userEvent.type(screen.getByPlaceholderText(/add a note/i), 'dinner')
+
+  expect(await screen.findByRole('button', {name: /pay/i})).toBeEnabled();
+})
