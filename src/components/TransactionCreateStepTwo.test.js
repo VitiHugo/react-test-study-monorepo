6dc3a61d16cb288a2143fda0f77e5b678cbb1e64
Vitor Hugo <vitork42@gmail.com>
2022-04-17 05:58:31 -0300

    :rotating_light: Studies completed

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -2,15 +2,29 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import TransactionCreateStepTwo from './TransactionCreateStepTwo'
 
-
-test('on initial render, the pay button is disabled', async () => {
-  var sender = {id: 5}
-  var receiver = {id: '5'}
+//Unit tests
+// test('on initial render, the pay button is disabled', async () => {
+//   var sender = {id: 5}
+//   var receiver = {id: '5'}
   
-  render(<TransactionCreateStepTwo sender={sender} receiver={receiver} />);
+//   render(<TransactionCreateStepTwo sender={sender} receiver={receiver} />);
 
-  expect(await screen.findByRole('button', {name: /pay/i})).toBeDisabled();
-})
+//   expect(await screen.findByRole('button', {name: /pay/i})).toBeDisabled();
+// })
+
+// test('if an amount and note is entered, the pay button becomes enabled', async () => {
+//   var sender = {id: 5}
+//   var receiver = {id: '5'}
+  
+//   render(<TransactionCreateStepTwo sender={sender} receiver={receiver} />);
+
+//   userEvent.type(screen.getByPlaceholderText(/amount/i), '50')
+//   userEvent.type(screen.getByPlaceholderText(/add a note/i), 'dinner')
+
+//   expect(await screen.findByRole('button', {name: /pay/i})).toBeEnabled ();
+// })
+
+//Integration tests
 
 test('if an amount and note is entered, the pay button becomes enabled', async () => {
   var sender = {id: 5}
@@ -18,8 +32,10 @@ test('if an amount and note is entered, the pay button becomes enabled', async (
   
   render(<TransactionCreateStepTwo sender={sender} receiver={receiver} />);
 
+  expect(await screen.findByRole('button', {name: /pay/i})).toBeDisabled();
+
   userEvent.type(screen.getByPlaceholderText(/amount/i), '50')
   userEvent.type(screen.getByPlaceholderText(/add a note/i), 'dinner')
 
-  expect(await screen.findByRole('button', {name: /pay/i})).toBeEnabled();
+  expect(await screen.findByRole('button', {name: /pay/i})).toBeEnabled ();
 })
